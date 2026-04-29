1a9b503f13e3d42bc2726c656f23cbb6ce897c9c
alessandrolill0 <alessandro.lillo@hotmail.it>
2026-04-29 17:08:15 +0200

    modified stft_weight

--- src/train.py
+++ src/train.py
@@ -28,7 +28,7 @@ def main():
 
     # 2. INITIALIZE MODULES
     model = DifferentiableModalPlate(sample_rate=sample_rate, plate_params=None, dtype=dtype).to(device)
-    criterion = TimeDomainEnergyLoss(mse_weight=1.0, stft_weight=50.0, energy_weight=1.0).to(device)
+    criterion = TimeDomainEnergyLoss(mse_weight=1.0, stft_weight=20.0, energy_weight=1.0).to(device)
 
     # Initialize Adam Optimizer
     # We use custom learning rates
