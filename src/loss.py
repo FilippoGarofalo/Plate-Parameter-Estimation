07a15d121f5e130138eecd87a78468cdf72dcea1
Filippo Garofalo <filippo.garofalo@mail.polimi.it>
2026-05-05 22:46:22 +0200

    Revised loss

--- src/loss.py
+++ src/loss.py
@@ -5,27 +5,23 @@ import torch.nn.functional as F
 class Loss(nn.Module):
 
     def __init__(self, mse_weight=0.0, stft_weight=1.0,
-                 energy_weight=1.0, lowpass_weight=10.0,
                  cutoff_hz=200.0, sr=44100,
                  fft_sizes=[64, 256, 1024, 4096]):
         super().__init__()
 
         self.mse_weight = mse_weight
         self.stft_weight = stft_weight
-        self.energy_weight = energy_weight
-        self.lowpass_weight = lowpass_weight
 
         self.cutoff_hz = cutoff_hz
         self.sr = sr
         self.fft_sizes = fft_sizes
 
-        # cache finestre (IMPORTANTISSIMO)
         self.windows = nn.ParameterDict({
             str(n): nn.Parameter(torch.hann_window(n), requires_grad=False)
             for n in fft_sizes
         })
 
-        self.eps = 1e-7
+        self.eps = 1e-4
 
     def forward(self, pred_audio, target_audio):
 
@@ -80,35 +76,12 @@ class Loss(nn.Module):
         mss_loss = mss_loss / len(self.fft_sizes)
 
         # =========================
-        # 4. LOWPASS LOSS (SMOOTH)
-        # =========================
-        N = pred_audio.shape[-1]
-
-        freqs = torch.fft.rfftfreq(N, d=1.0/self.sr).to(device)
-
-        # smooth mask (differenziabile)
-        cutoff = torch.tensor(self.cutoff_hz, device=device)
-        mask = torch.sigmoid((cutoff - freqs) / 5.0)
-
-        pred_fft = torch.fft.rfft(pred_audio)
-        target_fft = torch.fft.rfft(target_audio)
-
-        pred_mag = torch.abs(pred_fft).clamp_min(self.eps)
-        target_mag = torch.abs(target_fft).clamp_min(self.eps)
-
-        lowpass_loss = F.l1_loss(
-            torch.log(pred_mag) * mask,
-            torch.log(target_mag) * mask
-        )
-
-        # =========================
         # FINAL
         # =========================
         total_loss = (
             self.mse_weight * mse_loss +
             self.stft_weight * mss_loss +
-            self.energy_weight * energy_loss +
-            self.lowpass_weight * lowpass_loss
+            self.energy_weight * energy_loss
         )
 
         return total_loss
