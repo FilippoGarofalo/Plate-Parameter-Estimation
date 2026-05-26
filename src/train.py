6417ceeb83aaf191f8bbc7197b513f3d5bb031f6
alessandrolill0 <alessandro.lillo@hotmail.it>
2026-05-26 09:53:17 +0200

    train modified

--- src/train.py
+++ src/train.py
@@ -107,9 +107,9 @@ def main():
     progress = {'iteration': [], 'loss': [], 'mu': [], 'D_over_mu': [], 'T0_over_mu': [], 'Ly': [], 'xo': [], 'yo': []}
 
     STFT_DURATION = 1.0        
-    MSE_DURATION = duration - 0.05  # dynamic safety margin to avoid file-end clipping
+    MSE_DURATION = min(duration - 0.05, 1)  # dynamic safety margin to avoid file-end clipping
     use_mse = False
-    mse_start_iter = None         
+    mse_start_iter = None
 
     # 3. OPTIMIZATION LOOP
     print("\nStarting Optimization")
@@ -123,7 +123,7 @@ def main():
 
         ### MODIFIED: Restored the correct curriculum logic ###
         if not use_mse:
-            curr_duration = min(0.05 + (iteration/1000)*STFT_DURATION, STFT_DURATION)
+            curr_duration = min(0.05 + (iteration/300)*STFT_DURATION, STFT_DURATION)
         else:
             mse_iters_elapsed = iteration - mse_start_iter
             curr_duration = min(STFT_DURATION + (mse_iters_elapsed / 500) * (MSE_DURATION - STFT_DURATION), MSE_DURATION)
